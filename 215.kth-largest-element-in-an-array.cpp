9f9919bdeb576bf4591cdfd7c3d663ceb4c182cd
Hang Yang <yangdehang0204@gmail.com>
2021-04-09 11:15:16 -0400

    Min heap with user defined type

--- 215.kth-largest-element-in-an-array.cpp
+++ 215.kth-largest-element-in-an-array.cpp
@@ -11,6 +11,7 @@ public:
         //for kth largest problem think of heap
         //priority_queue by default is a max heap
         //using the following to make it a min heap
+        //heap sort is online algo while sorting is offline
         priority_queue<int, vector<int>,greater<int>> pq;
         for(int i = 0; i < nums.size(); i++){
             //maintain a size k heap
