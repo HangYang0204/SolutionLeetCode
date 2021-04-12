c4fa3cbe66c92be926481f89f477c02756c91575
Hang Yang <hang.yang@siriusxm.ca>
2021-04-12 18:31:35 -0400

    using algorithm.min_element

--- TrainSchedule.cpp
+++ TrainSchedule.cpp
@@ -18,30 +18,28 @@ public:
 		ScheduleByTrain.insert({1, {0,15,25,40,60}});
 		ScheduleByTrain.insert({2, {10,-1,30,-1,50}});
 		ScheduleByTrain.insert({3, {15,-1,-1,-1,55}});	
-		std::vector<int> TrainArrival;
+		std::vector<int> TrainArrival = {100};
 		for(int i = 0; i < 3; i++){
 			std::vector<int> Schedule = ScheduleByTrain[i + 1];
-			if(Schedule[toStop] > 0){
-				TrainArrival.push_back(Schedule[toStop]);
+			//make sure it is a valid path 
+			if(Schedule[fromStop - 1] != -1 && Schedule[toStop - 1] != -1){
+				TrainArrival.push_back(Schedule[toStop - 1]);
 			}
 		}
-		 
-		int TrainIdx = 1 + std::min_element(TrainArrival.begin(),TrainArrival.end()) - TrainArrival.begin();
+		
+		auto TrainIdx = std::min_element(TrainArrival.begin(),TrainArrival.end()) - TrainArrival.begin();
 		int minScheduleTime = *std::min_element(TrainArrival.begin(),TrainArrival.end());
 		memo[TrainIdx] = minScheduleTime;
+		// for(int v : TrainArrival){
+		// 	std::cout << v << std::endl;
+		// }
 		return TrainIdx;
 		
 	}
 private:
 	std::unordered_map<int, int> memo;//stores (train with minimal arrival time : stop)
 };
-
-class SolutionQ2{
-public:
-
-
-};
-
+ 
 int main(){
 	SolutionQ1 s;
 	int fromstop;
@@ -51,5 +49,6 @@ int main(){
 	std::cout << "Now type in the end stop number(2-5):" << std::endl;
 	std::cin >> tostop;
 	std::cout << s.TrainToTake(fromstop,tostop) << std::endl;
+	//s.TrainToTake(fromstop,tostop);
 	return 0;
 }
