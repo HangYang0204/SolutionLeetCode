b4b646d8a94b13ac4cf2167c57f479e9badd3446
Hang Yang <yangdehang0204@gmail.com>
2021-04-19 20:40:41 -0400

    added git ignore

--- Algo_TreeTraversal.cpp
+++ Algo_TreeTraversal.cpp
@@ -1,33 +1,60 @@
 #include<vector>
 #include<iostream>
+#include<stack>
+#include<queue>
+using std::queue;
+using std::stack;
 
-struct TreeNode{
+class TreeNode{
+public:
     int val;
-    struct TreeNode* root;
-    struct TreeNode* left;
-    struct TreeNode* right;
-    //TreeNode(): val(0), left(nullptr), right(nullptr){}
-    TreeNode(){
-        this->val = 0;
-        this->left = nullptr;
-        this->right = nullptr;
-    }
-    //TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x){
-        this->val = x;
-        this->left = nullptr;
-        this->right = nullptr;
-    }
-    //TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-    TreeNode(int x, TreeNode* left, TreeNode* right){
-        this->val = x;
-        this->left = left;
-        this->right = right;
-    }
+    TreeNode* root;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(): val(0), left(nullptr), right(nullptr){}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-class Solution{
+class TreeTraversal{
     public:
+    //print a tree
+    void PrintBinaryTree(TreeNode* root){
+        if(root != nullptr){
+            std::cout << root->val << std::endl;
+            if(root->left != nullptr) PrintBinaryTree(root->left);
+            if(root->right != nullptr) PrintBinaryTree(root->right);
+        } 
+        else return;
+    }
+    //traversal a tree DFS
+    void DFSBinaryTree(TreeNode* root){
+        stack<TreeNode*> sk;
+        if(root != nullptr) sk.push(root);
+        while(!sk.empty()){
+            TreeNode* curr = sk.top();
+            std::cout << curr->val << std::endl;
+            sk.pop();
+            if(curr->right != nullptr) sk.push(curr->right);
+            if(curr->left != nullptr) sk.push(curr->left);     
+        }
+    }
+    //traversal by layer 
+    void BinaryTreeTraversal(TreeNode* root){
+        queue<TreeNode*> q;
+        if(not root) return;
+        q.push(root);
+        while(!q.empty()){
+            int size = q.size();
+            for(int i = 0; i< size; i++){
+                TreeNode *curr = q.front();
+                q.pop();
+                std::cout << curr->val << std::endl;
+                if(curr->left != nullptr) q.push(curr->left);
+                if(curr->right != nullptr) q.push(curr->right);
+            }                
+        }
+    }
     std::vector<int> inordertraversal(TreeNode* root){
         treeTraversal(root);
         return result;
@@ -44,15 +71,30 @@ class Solution{
     std::vector<int> result;
 };
 
+void Test1(){
+    TreeNode* x = new TreeNode(3);
+    TreeNode* x2 = new TreeNode(6);
+    TreeNode* y = new TreeNode(5,x,x2);
+    TreeNode* x3 = new TreeNode(9);
+    TreeNode* y2 = new TreeNode(7,x3,nullptr);
+    TreeNode* z = new TreeNode(1,y2,y);
+    TreeTraversal ts;
+    //ts.PrintBinaryTree(z);
+    //ts.DFSBinaryTree(z);
+    ts.BinaryTreeTraversal(z);
+}
+
 int main(){
     TreeNode* x = new TreeNode(3);
     TreeNode* y = new TreeNode(5,x,nullptr);
     TreeNode* z = new TreeNode(1,nullptr,y);
-    Solution s;
+    TreeTraversal s;
     std::vector<int> res;
-    res = s.inordertraversal(z);
-    for(int i : res){
-        std::cout << i << std::endl;
-    }
+    // res = s.inordertraversal(z);
+    // for(int i : res){
+    //     std::cout << i << std::endl;
+    // }
+
+    Test1();
  
 }
